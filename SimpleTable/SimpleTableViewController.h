878a647d13a9ee054ab866f459a142adb2a80bd0
Orakaro <nhatminh_179@hotmail.com>
2012-06-18 11:35:00 +0700

    Initial Commit

---
+++ SimpleTable/SimpleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SimpleTableViewController.h
+//  SimpleTable
+//
+//  Created by Minh Vu Nhat on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTableViewController : UIViewController
+
+@end
